92e7b8ed1e124fc070f199b18335aacc79bdf1c9
voidzero <kartikjagdale11@gmail.com>
2017-03-24 19:07:07 +0530

    upgraded dependenies

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -1,10 +1,12 @@
+import { environment } from './../environments/environment';
 import { CustomConfig } from 'ng2-ui-auth';
 /**
  * Created by VoidZero on 09/02/2017.
  */
 
 export const GOOGLE_CLIENT_ID = '64504024589-1oukh9057md9e2g1rrads4ppufdv3ge2.apps.googleusercontent.com';
-const baseUrl = "http://localhost:3000/api/auth/google"
+const baseUrl = environment.API_ENDPOINT + '/auth/google';
+
 export class MyAuthConfig extends CustomConfig {
     defaultHeaders = {'Content-Type': 'application/json'};
     providers = {google: { clientId: GOOGLE_CLIENT_ID, url: baseUrl }};
