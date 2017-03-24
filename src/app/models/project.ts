73e4c4be175a8e10887eb4a6b0f1a62702de6988
voidzero <kartikjagdale11@gmail.com>
2017-03-24 14:52:24 +0530

    made ui changes to project list

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,12 +1,13 @@
 import { Base } from './base';
 export class Project extends Base {
-  git_url     :string
-  description :string
-  demo_url    :string
-  author_name :string
-  name        :string
-  caption     :string
-  twitter_id  :string
-  approved    :boolean
-  created_at  :string
+  git_url: string;
+  description: string;
+  demo_url: string;
+  image_url: string;
+  author_name: string;
+  name: string;
+  caption: string;
+  twitter_id: string;
+  approved: boolean;
+  created_at: string;
 }
