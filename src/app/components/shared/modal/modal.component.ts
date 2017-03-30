9704c7c69e38f16b4a234e932ff6b75cc61d0d35
voidzero <kartikjagdale11@gmail.com>
2017-03-30 12:19:34 +0530

    Add Project detail Modal and Fix Ui

--- src/app/components/shared/modal/modal.component.ts
+++ src/app/components/shared/modal/modal.component.ts
@@ -1,20 +1,22 @@
 import { ModalDirective } from 'ng2-bootstrap/ng2-bootstrap';
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit, AfterViewInit {
+export class ModalComponent implements AfterViewInit {
   @ViewChild('lgModal') public lgModal: ModalDirective;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private location: Location) { }
 
   ngAfterViewInit() {
     this.lgModal.show();
+  }
+  closeModal() {
+    this.lgModal.hide();
+    this.location.back();
   }
 }
